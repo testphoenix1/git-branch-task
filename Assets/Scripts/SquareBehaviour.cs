d46e904b98eceb38025def2ae67b2fe6f2a61e10
SierraTeachesStuff <sierra@learncoach.com>
2020-04-08 18:59:26 +1200

    initial commit

---
+++ Assets/Scripts/SquareBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SquareBehaviour : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Called whenever the mouse is over the collider
+    void OnMouseOver()
+    {
+
+    }
+}
