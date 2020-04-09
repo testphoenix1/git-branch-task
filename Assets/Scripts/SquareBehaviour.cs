5d3a1a2f325b562062be1b84e968f613c74417f6
SierraTeachesStuff <sierra@learncoach.com>
2020-04-09 20:21:50 +1200

    Update SquareBehaviour.cs

--- Assets/Scripts/SquareBehaviour.cs
+++ Assets/Scripts/SquareBehaviour.cs
@@ -2,15 +2,34 @@
 
 public class SquareBehaviour : MonoBehaviour
 {
+    bool isHeld;
+
     // Update is called once per frame
     void Update()
     {
-
+        if (isHeld)
+        {
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                isHeld = false;
+            }
+            else
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit hitInfo))
+                {
+                    transform.position = hitInfo.point;
+                }
+            }
+        }
     }
 
     // Called whenever the mouse is over the collider
     void OnMouseOver()
     {
-
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            isHeld = true;
+        }
     }
 }
